a78a22dfdec2d3b4f5bde6337975b14b00ddf49a
Robert Bain <31903812+bainro@users.noreply.github.com>
2021-05-26 15:46:46 -0700

    Update losses.py

--- losses.py
+++ losses.py
@@ -79,7 +79,8 @@ def focal(alpha=0.25, gamma=1.5):
         label_sums = tf.math.reduce_sum(labels, axis=-1)
         indices = tf.where(keras.backend.equal(label_sums, 1))
         # @TODO remove and add general solution for nuscenes training
-        labels = labels[indices, :].assign(tf.constant([1, 0, 0, 0, 0, 0, 0, 0]))
+        #labels = labels[indices, :].assign(tf.constant([1, 0, 0, 0, 0, 0, 0, 0]))
+        labels = labels[indices].assign(tf.constant([1, 0, 0, 0, 0, 0, 0, 0]))
         #single_class_labels = tf.zeros_like(labels)
         #.assign(tf.zeros(4))
         #labels = tf.Print(label_sums, [tf.shape(label_sums)], message=" label_sums.shape: ", summarize=-1)
