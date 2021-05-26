551e80407b5f75006b859d9ce2fa1e7035ba11ca
Robert Bain <31903812+bainro@users.noreply.github.com>
2021-05-26 14:56:48 -0700

    Update losses.py

--- losses.py
+++ losses.py
@@ -76,7 +76,8 @@ def focal(alpha=0.25, gamma=1.5):
         # filter out "ignore" anchors
         indices = tf.where(keras.backend.not_equal(anchor_state, -1))
         labels = tf.gather_nd(labels, indices)
-        labels = tf.Print(labels, [tf.shape(labels)], message=" labels.shape: ", summarize=-1)
+        label_sums = tf.math.reduce_sum(labels, axis=0)
+        label_sums = tf.Print(label_sums, [tf.shape(label_sums)], message=" label_sums.shape: ", summarize=-1)
         classification = tf.gather_nd(classification, indices)
 
         # compute the focal loss
