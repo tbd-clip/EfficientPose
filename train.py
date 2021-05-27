f89892e16eccbf8eb27a5bd4a140c02dc5e45a56
Robert Bain <31903812+bainro@users.noreply.github.com>
2021-05-27 11:39:29 -0700

    Update train.py

--- train.py
+++ train.py
@@ -376,7 +376,7 @@ def create_generators(args):
     elif args.dataset_type == 'occlusion1':
         from generators.occlusion_1_class import OcclusionGenerator1
         train_generator = OcclusionGenerator1(
-            args.occlusion_path,
+            args.occlusion1_path,
             rotation_representation = args.rotation_representation,
             use_colorspace_augmentation = not args.no_color_augmentation,
             use_6DoF_augmentation = not args.no_6dof_augmentation,
@@ -384,7 +384,7 @@ def create_generators(args):
         )
 
         validation_generator = OcclusionGenerator1(
-            args.occlusion_path,
+            args.occlusion1_path,
             train = False,
             shuffle_dataset = False,
             shuffle_groups = False,
