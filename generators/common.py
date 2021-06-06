6502cc0fcf8d28e81ce303da41326914a6629661
Robert Bain <31903812+bainro@users.noreply.github.com>
2021-06-06 13:36:47 -0700

    Update common.py

--- generators/common.py
+++ generators/common.py
@@ -360,7 +360,7 @@ class Generator(keras.utils.Sequence):
         for i in range(num_annos):
             rotation_matrix_annos[i, :, :] = self.axis_angle_to_rotation_mat(annotations["rotations"][i, :3])
             translation_vector_annos[i, :] = annotations["translations"][i, :]
-            bbox_annos[i, :] = annotations["bbox"][i, :]
+            bbox_annos[i, :] = annotations["bboxes"][i, :]
         
         #generate random scale and angle
         scale_range, min_scale = self.get_scale_6DoF_augmentation_parameter()
