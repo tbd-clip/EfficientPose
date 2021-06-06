5d019b01b0e05a86713825ae2b3e556220834455
Robert Bain <31903812+bainro@users.noreply.github.com>
2021-06-06 14:18:50 -0700

    Update losses.py

--- losses.py
+++ losses.py
@@ -187,7 +187,7 @@ def transformation_loss(model_3d_points_np, bcube_priors_np, num_rotation_parame
         regression_target_translation = y_true[:, :, num_rotation_parameter:-3]
         is_symmetric = y_true[:, :, -27]
         class_indices = y_true[:, :, -26]
-        flat_bcube = tf.cast(tf.math.round(y_true[:, :, -25:-1]), tf.float32)
+        flat_bcube = tf.Tensor(y_true[:, :, -25:-1], tf.float32)
         anchor_state      = tf.cast(tf.math.round(y_true[:, :, -1]), tf.int32)
     
         # filter out "ignore" anchors
@@ -199,6 +199,7 @@ def transformation_loss(model_3d_points_np, bcube_priors_np, num_rotation_parame
         
         regression_target_rotation = tf.gather_nd(regression_target_rotation, indices) * math.pi
         regression_target_translation = tf.gather_nd(regression_target_translation, indices)
+        _flat_cube = tf.gather_nd(flat_cube, indices)
         is_symmetric = tf.gather_nd(is_symmetric, indices)
         is_symmetric = tf.cast(tf.math.round(is_symmetric), tf.int32)
         class_indices = tf.gather_nd(class_indices, indices)
@@ -220,7 +221,7 @@ def transformation_loss(model_3d_points_np, bcube_priors_np, num_rotation_parame
         regression_translation = tf.expand_dims(regression_translation, axis = 1)
         regression_target_translation = tf.expand_dims(regression_target_translation, axis = 1)
         
-        gt_bcube_pre_Rt = tf.reshape(flat_bcube, tf.shape(bcube_prior_points))
+        gt_bcube_pre_Rt = tf.reshape(_flat_bcube, tf.shape(bcube_prior_points))
         
         transformed_points_pred = rotate(bcube_prior_points * regression_scaling, axis_pred, angle_pred) + regression_translation
         transformed_points_target = rotate(gt_bcube_pre_Rt, axis_target, angle_target) + regression_target_translation
